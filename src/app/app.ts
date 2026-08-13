0abe4274b6834162890b777a8b4bd5a92ff8c7ea
Djim <dbadjidk@groupeisi.com>
2026-08-13 18:02:16 +0000

    chore: OnPush racine + suppression fichiers parasites et mise a jour du rapport

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toasts } from './shared/components/toasts/toasts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toasts],   // ← Toasts ajouté ici
+  imports: [RouterOutlet, Toasts],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,   // ← ajouté
 })
 export class App {}
