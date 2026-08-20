f2a9e9834f4b62a5af43b0d57a039ec398c23256
FOUADI Said <saidfouadisaid@gmail.com>
2026-08-20 07:03:31 +0000

    feat(b2): activation <withComponentInputBinding> pour les routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))
   ]
 };
